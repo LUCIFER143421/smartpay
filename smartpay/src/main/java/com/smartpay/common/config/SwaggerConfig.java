25722585c04002eaf42078e6c2cd0cdb4e1982a6
Nikhil <nikhilsinghrajpoot04@gmail.com>
2026-07-28 13:16:27 +0530

    Add SwaggerConfig with production server URL

---
+++ smartpay/src/main/java/com/smartpay/common/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.smartpay.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI smartPayOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SmartPay API")
+                        .description("Production-grade Fintech Payment System")
+                        .version("1.0.0"))
+                .servers(List.of(
+                        new Server()
+                                .url("https://smartpay-production-9274.up.railway.app")
+                                .description("Production"),
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Local")
+                ));
+    }
+}
