829f2ede9e578a78486fee8b783fbf040b1dccb5
Nikhil <nikhilsinghrajpoot04@gmail.com>
2026-07-29 16:19:48 +0530

    Added jwt option in swagger

--- smartpay/src/main/java/com/smartpay/common/config/SwaggerConfig.java
+++ smartpay/src/main/java/com/smartpay/common/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.smartpay.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +30,16 @@ public class SwaggerConfig {
                         new Server()
                                 .url("http://localhost:8080")
                                 .description("Local")
-                ));
+                ))
+                // This adds the Authorize button to Swagger UI
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Paste your JWT token here")));
     }
 
     @Bean
