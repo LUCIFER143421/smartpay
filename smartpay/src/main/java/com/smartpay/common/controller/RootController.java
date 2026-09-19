47896f9644f958ef49bd5883f4dae34548f4399f
Nikhil Kumar Singh <nikhilsinghrajpoot04@gmail.com>
2026-09-19 23:26:09 +0530

    Serve SmartPay frontend at root

---
+++ smartpay/src/main/java/com/smartpay/common/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.smartpay.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+}
